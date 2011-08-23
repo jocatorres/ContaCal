9358277738fd40339ad000758cc9bcd6d5ef3aef
Sylvestre Mergulhão <contato@mergulhao.info>
2011-08-23 11:25:03 -0300

    move default mailer url to application

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,7 @@ module Contacal
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    config.action_mailer.default_url_options = { :host => 'app.contacal.com.br' }
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,4 @@ Contacal::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
-
-  config.action_mailer.default_url_options = { :host => 'app.contacal.com.br' }
 end
