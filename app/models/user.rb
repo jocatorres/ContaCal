12417523ba2a3c7917a27bbeb12fe498a06b9ac5
Sylvestre Mergulhão <contato@mergulhao.info>
2011-08-23 11:06:01 -0300

    create devise model user

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+end
