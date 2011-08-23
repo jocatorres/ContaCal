690047f5925fb8ba223ebb7c77b4cbc2092c307b
Rafael Lima <contato@rafael.adm.br>
2011-08-23 16:45:28 -0300

    configuration for smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,4 +49,13 @@ Contacal::Application.configure do
   config.active_support.deprecation = :notify
   
   config.action_mailer.default_url_options = { :host => 'app.contacal.com.br' }
+  
+  config.action_mailer.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 end
