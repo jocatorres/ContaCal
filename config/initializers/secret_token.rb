1fdb493708acf3895c8c5544779c0e699249b6c0
Sylvestre Mergulhão <contato@mergulhao.info>
2011-08-23 10:12:32 -0300

    brand new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Contacal::Application.config.secret_token = '99964ba93923709a6c3f997d53fffa0a4bf87917d1b059a2a93e3d77d72a8152583caaa81a1f6e39b601f80689c2aa47b607d93d5b1b36990db023f0dd044aae'
