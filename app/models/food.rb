3ed4bbc73c6b560c38f30dbc5bdf93da8dab2875
Rafael Lima <contato@rafael.adm.br>
2011-08-23 14:53:37 -0300

    refactoring of food validations syntax

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,11 +1,8 @@
 # -*- encoding : utf-8 -*-
 class Food < ActiveRecord::Base
   has_many :user_foods
-  validates_presence_of :name
-  validates_presence_of :weight
-  validates_presence_of :measure
-  validates_presence_of :kcal
-  validates_inclusion_of :kind, :in => %w( a b c )
+  validates :name, :weight, :measure, :kcal, :presence => true
+  validates :kind, :inclusion => %w( a b c )
   
   attr_accessible :name, :weight, :measure, :kcal, :kind
 end
