2f0371751d357f65fccd19b53239e39cd7787f0c
RamonPage <ramonpage@gmail.com>
2011-08-23 15:34:37 -0300

    Overriding field_error_proc.

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,8 @@ module Contacal
     # JavaScript files you want as :defaults (application.js is always included).
     # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| "<span class=\"field_with_errors\">#{html_tag}</span>".html_safe }
+
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
 
