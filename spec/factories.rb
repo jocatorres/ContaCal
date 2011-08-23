f5d9a048cd433aeba808d9fbdff8a7aa0ac86d09
Rafael Lima <contato@rafael.adm.br>
2011-08-23 12:12:26 -0300

    come back with ut8 chars on factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 FactoryGirl.define do
   factory :food do
-    name 'Abacate batido com limao e acucar'
+    name 'Abacate batido com limão e açúcar'
     weight '90 Gramas - g'
     measure '2 Colher de sopa'
     kcal 164
