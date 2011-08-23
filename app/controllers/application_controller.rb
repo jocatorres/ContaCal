5bd410ed30715455ab47e7562b3faa08f83dd447
RamonPage <ramonpage@gmail.com>
2011-08-23 16:33:38 -0300

    Adding layout by resource.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  layout :layout_by_resource
+  
   def index
     render :inline => "alow", :layout => "application"
+  end
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
   end
 end
