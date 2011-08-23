e5543dbcd66503584a6cfa4d44d57b66ab7cf271
Rafael Lima <contato@rafael.adm.br>
2011-08-23 14:33:28 -0300

    migration to populate foods table

---
+++ db/migrate/20110823172654_populate_foods.rb
@@ -0,0 +1,19 @@
+class PopulateFoods < ActiveRecord::Migration
+  def self.up
+    lines = File.open(Rails.root.join('db','foods.txt')).readlines
+    lines.each do |line|
+      p = line.split("|")
+      Food.create!({
+        :name => p[0],
+        :weight => p[1],
+        :measure => p[2],
+        :kcal => p[3],
+        :kind => p[4],
+      })
+    end
+  end
+
+  def self.down
+    Food.delete_all
+  end
+end
