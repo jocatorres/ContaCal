3e41b632569d121a9606ccfec42040e809defc90
Rafael Lima <contato@rafael.adm.br>
2011-08-24 16:25:12 -0300

    email sent on end of day

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -9,4 +9,11 @@ class NotificationMailer < ::ActionMailer::Base
       :subject => "Resumo de suas calorias em #{I18n.l(@date)}")
   end
 
+  def end_of_day(user)
+    @user = user
+    @date = Date.today
+    mail(:to => "#{user.name} <#{user.email}>",
+      :subject => "Resumo de suas calorias de hoje (#{I18n.l(@date)})")
+  end
+
 end
