34d2fc82ad99e55f4a1e00583ced63b099de1fa4
Sylvestre Mergulhão <contato@mergulhao.info>
2011-08-24 17:01:31 -0300

    refactoring on mail senders

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,17 @@ class User < ActiveRecord::Base
   validates :name, :presence => true
   scope :subscribed, where(:subscribed => true)
 
-  def self.send_beginning_of_day_notification!
-    subscribed.each do |user|
-      NotificationMailer.beginning_of_day(user).deliver
+  class << self
+    def send_beginning_of_day_notification!
+      run_on_each_subscribed { |user| NotificationMailer.beginning_of_day(user).deliver }
     end
-  end
 
-  def self.send_end_of_day_notification!
-    subscribed.each do |user|
-      NotificationMailer.end_of_day(user).deliver
+    def send_end_of_day_notification!
+      run_on_each_subscribed { |user| NotificationMailer.end_of_day(user).deliver }
+    end
+    
+    def run_on_each_subscribed
+      subscribed.each { |user| yield(user) }
     end
   end
 
