99306445e36faf9e297882cf8d68bb2cf0f01c9b
Rafael Lima <contato@rafael.adm.br>
2011-08-24 11:40:08 -0300

    insert jquery.debug

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,3 +5,11 @@ $(function() {
     $("#change-kcal-label").hide();
   });
 });
+
+var debug = new $.debug({
+	posTo : { x:'right', y:'bottom' },
+	width: '280px',
+	height: '200px',
+	itemDivider : '<hr>',
+	listDOM : [ 'tagName','id', 'innerText', 'href' ]
+});
