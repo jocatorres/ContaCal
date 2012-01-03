5a0e249c8f33efa55b7a73d92c34c2e63da53c88
Joca Torres <jtorres@jig.com.br>
2012-01-03 10:01:44 -0200

    Mudando forma de fazer sugestão de alimentos.

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -13,7 +13,7 @@ class FoodsController < ApplicationController
     if !@food.name.blank?
 #      NotificationMailer.new_food(current_user, @food).deliver
       @food.update_attribute(:kind, "h")      
-#      @food.update_attribute(:suggester_id, current_user.id)
+      @food.update_attribute(:suggester_id, current_user.id)
       @food.save
       redirect_to new_food_path, notice: 'Sua sugestão foi enviada com sucesso!'
     else
