0f088a27929a79e5f5a5907e7d1f3a217fb58a17
Joca Torres <jtorres@jig.com.br>
2012-04-18 09:03:11 -0300

    Adicionando coluna de nutricionista na tabela users

---
+++ db/migrate/20120418120022_add_nutri_mail_and_name_to_users.rb
@@ -0,0 +1,11 @@
+class AddNutriMailAndNameToUsers < ActiveRecord::Migration
+  def self.up
+    add_column :users, :nutri_email, :string
+    add_column :users, :nutri_name, :string
+  end
+
+  def self.down
+    remove_column :users, :nutri_name
+    remove_column :users, :nutri_email
+  end
+end
