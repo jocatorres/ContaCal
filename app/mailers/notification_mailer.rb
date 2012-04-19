618db91f271a5e1540e113673ec10142164dccce
Joca Torres <jtorres@jig.com.br>
2012-04-19 14:49:49 -0300

    Ajustando task diária.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -13,13 +13,9 @@ class NotificationMailer < ::ActionMailer::Base
 
   def weekly(user)
     @user = user
-    puts "Mandando email semanal"
-    puts "weekly: mail(:to => #{user.name} <#{user.email}>,..."
     mail(:to => "#{user.name} <#{user.email}>",
       :subject => "Resumo semanal de calorias consumidas")
     if (!user.nutri_email.blank?) 
-      puts "Tem nutri!"
-      puts "weekly - nutri: mail(:to => #{user.nutri_name} <#{user.nutri_email}>,..."
       mail(:to => "#{user.nutri_name} <#{user.nutri_email}>",  
         :subject => "Resumo semanal de calorias consumidas de #{user.name} (#{user.email})")
     end
@@ -30,27 +26,19 @@ class NotificationMailer < ::ActionMailer::Base
     @date = 1.day.ago.to_date
     puts "[ContaCal] Resumo das calorias de [#{user.name}] [#{user.email}] em [#{I18n.l(@date)}] - nutri = [#{user.nutri_name}] [#{user.nutri_email}]."
     if (user.status == 1 or user.status == 10)
-      puts "Eh trial."
-      puts "daily - trial: mail(:to => #{user.name} <#{user.email}>,..."
-#      mail(:to => "#{user.name} <#{user.email}>",
-      mail(:to => "Joca <jtorres@jig.com.br>",
+#      mail(:to => "Joca <jtorres@jig.com.br>",
+      mail(:to => "#{user.name} <#{user.email}>",
         :subject => "Está gostando do ContaCal?")
-      puts "enviou..."
       if (user.expire_at < Date.today-1)
         user.update_attribute(:subscribed_daily, "f")
       end
-      puts "analisou expire_at"
     else  
-      puts "daily - nao trial: mail(:to => #{user.name} <#{user.email}>,..."
-#      mail(:to => "#{user.name} <#{user.email}>",
-      mail(:to => "Joca <jtorres@jig.com.br>",
+#      mail(:to => "Joca <jtorres@jig.com.br>",
+      mail(:to => "#{user.name} <#{user.email}>",
         :subject => "[ContaCal] Resumo de suas calorias em #{I18n.l(@date)}")
-      puts "Nao eh trial. Vamos ver se tem nutri. [#{user.nutri_name}] [<#{user.nutri_email}]"
       if (!user.nutri_email.blank?) 
-         puts "Tem nutri!"
-         puts "daily - nao trial - nutri: mail(:to => #{user.nutri_name} <#{user.nutri_email}>,..."
-#         mail(:to => "#{user.nutri_name} <#{user.nutri_email}>",
-         mail(:to => "Joaquim <joaquim.torres@locaweb.com.br>",
+#         mail(:to => "Joaquim <joaquim.torres@locaweb.com.br>",
+         mail(:to => "#{user.nutri_name} <#{user.nutri_email}>",
           :subject => "[ContaCal] Resumo das calorias de #{user.name} (#{user.email}) em #{I18n.l(@date)}")
       end
     end
@@ -64,14 +52,14 @@ class NotificationMailer < ::ActionMailer::Base
 #      :subject => "Resumo de suas calorias de hoje (#{I18n.l(@date)})")
   end
 
-  def new_food(user, food)
-    @user = user
-    @food = food
-    puts "new_food: mail(:to => #{user.name} <#{user.email}>,..."
+#  def new_food(user, food)
+#    @user = user
+#    @food = food
+#    puts "new_food: mail(:to => #{user.name} <#{user.email}>,..."
 #    mail(
 #      :from => "#{user.name} <#{user.email}>",
 #      :to => "Info ContaCal <sugestao@contacal.com.br>",
 #      :subject => "Sugestão de novo alimento")
-  end
+#  end
 
 end
--- lib/tasks/cron.rake
+++ lib/tasks/cron.rake
@@ -9,11 +9,11 @@ namespace :notification do
 end
 
 task :cron => :environment do
-  if Time.zone.now.hour == 8 && Time.zone.now.wday == 1
+  if Time.zone.now.hour == 6 && Time.zone.now.wday == 1
     Rake::Task['notification:weekly'].invoke
   end
 
-  if Time.zone.now.hour == 12
+  if Time.zone.now.hour == 15
     Rake::Task['notification:beginning_of_day'].invoke
   end
 end
