07d7100b31ce15fed882ee1544311422e4aafe3b
Joca Torres <jtorres@jig.com.br>
2012-04-19 10:32:21 -0300

    Ajustando task diária.

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -6,6 +6,7 @@ class NotificationMailer < ::ActionMailer::Base
 
   def welcome(user)
     @user = user
+    puts "welcome: mail(:to => #{user.name} <#{user.email}>,..."
     mail(:to => "#{user.name} <#{user.email}>",
       :subject => "Seja bem vindo ao ContaCal!")    
   end
@@ -13,10 +14,12 @@ class NotificationMailer < ::ActionMailer::Base
   def weekly(user)
     @user = user
     puts "Mandando email semanal"
+    puts "weekly: mail(:to => #{user.name} <#{user.email}>,..."
 #    mail(:to => "#{user.name} <#{user.email}>",
 #      :subject => "Resumo semanal de calorias consumidas")
     if (!user.nutri_email.blank?) 
       puts "Tem nutri!"
+      puts "weekly - nutri: mail(:to => #{user.nutri_name} <#{user.nutri_email}>,..."
 #      mail(:to => "#{user.nutri_name} <#{user.nutri_email}>",  
 #        :subject => "Resumo semanal de calorias consumidas de #{user.name} (#{user.email})")
     end
@@ -27,18 +30,21 @@ class NotificationMailer < ::ActionMailer::Base
     @date = 1.day.ago.to_date
     puts "[ContaCal] Resumo das calorias de [#{user.name}] [#{user.email}] em [#{I18n.l(@date)}] - nutri = [#{user.nutri_name}] [#{user.nutri_email}]."
     if (user.status == 1 or user.status == 10)
-    puts "Eh trial."
+      puts "Eh trial."
+      puts "daily - trial: mail(:to => #{user.name} <#{user.email}>,..."
 #      mail(:to => "#{user.name} <#{user.email}>",
 #        :subject => "Está gostando do ContaCal?")
       if (user.expire_at < Date.today-1)
         user.update_attribute(:subscribed_daily, "f")
       end
     else  
+      puts "daily - nao trial: mail(:to => #{user.name} <#{user.email}>,..."
 #      mail(:to => "#{user.name} <#{user.email}>",
 #        :subject => "[ContaCal] Resumo de suas calorias em #{I18n.l(@date)}")
       puts "Nao eh trial. Vamos ver se tem nutri. [#{user.nutri_name}] [<#{user.nutri_email}]"
       if (!user.nutri_email.blank?) 
          puts "Tem nutri!"
+         puts "daily - nao trial - nutri: mail(:to => #{user.nutri_name} <#{user.nutri_email}>,..."
 #        mail(:to => "#{user.nutri_name} <#{user.nutri_email}>",
 #          :subject => "[ContaCal] Resumo das calorias de #{user.name} (#{user.email}) em #{I18n.l(@date)}")
       end
@@ -55,6 +61,7 @@ class NotificationMailer < ::ActionMailer::Base
   def new_food(user, food)
     @user = user
     @food = food
+    puts "new_food: mail(:to => #{user.name} <#{user.email}>,..."
     mail(
  #     :from => "#{user.name} <#{user.email}>",
       :to => "Info ContaCal <sugestao@contacal.com.br>",
