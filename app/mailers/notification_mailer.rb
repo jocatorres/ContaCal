be0fd5896be3f4d22c3dda8a85265ecaf2f87e1e
Joca Torres <jtorres@jig.com.br>
2012-04-21 14:15:40 -0300

    Email diário

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -14,48 +14,39 @@ class NotificationMailer < ::ActionMailer::Base
 
   def weekly(user)
     @user = user
-#    mail(
-#      :from => "Info ContaCal <info@contacal.com.br>",
-#      :to => "#{user.name} <#{user.email}>",
-#      :subject => "Resumo semanal de calorias consumidas")
-#    if (!user.nutri_email.blank?) 
-#      mail(
-#        :from => "Info ContaCal <info@contacal.com.br>",
-#        :to => "#{user.nutri_name} <#{user.nutri_email}>",  
-#        :subject => "Resumo semanal de calorias consumidas de #{user.name} (#{user.email})")
-#    end
+    mail(
+      :to => "#{user.name} <#{user.email}>",
+      :subject => "Resumo semanal de calorias consumidas")
+    if (!user.nutri_email.blank?) 
+      mail(
+        :to => "#{user.nutri_name} <#{user.nutri_email}>",  
+        :subject => "Resumo semanal de calorias consumidas de #{user.name} (#{user.email})")
+    end
   end
 
   def beginning_of_day(user)
     @user = user
     @date = 1.day.ago.to_date  
     puts "mandando para [#{user.email}] - nutri = [#{user.nutri_email}]"      
-#   if (user.email == "jtorres@jig.com.br")
-#           mail(
-#             :from => "Info ContaCal <info@contacal.com.br>",
-#             :to => "#{user.name} <#{user.email}>",
-#             :subject => "[ContaCal] Resumo de suas calorias em #{I18n.l(@date)}")
-#    if (user.status == 1 or user.status == 10)
-#      mail(
-#        :from => "Info ContaCal <info@contacal.com.br>",
-#        :to => "#{user.name} <#{user.email}>",
-#        :subject => "Está gostando do ContaCal?")
-#      if (user.expire_at < Date.today-1)
-#        user.update_attribute(:subscribed_daily, "f")
-#      end
-#    else  
-#      mail(
-#        :from => "Info ContaCal <info@contacal.com.br>",
-#        :to => "#{user.name} <#{user.email}>",
-#        :subject => "[ContaCal] Resumo de suas calorias em #{I18n.l(@date)}")
-#      if (!user.nutri_email.blank?) 
-#        mail(
-#          :from => "Info ContaCal <info@contacal.com.br>",
-#          :to => "#{user.nutri_name} <#{user.nutri_email}>",
-#          :subject => "[ContaCal] Resumo das calorias de #{user.name} (#{user.email}) em #{I18n.l(@date)}")
-#      end
-#    end
-#   end
+   if (user.email == "jtorres@jig.com.br")
+    if (user.status == 1 or user.status == 10)
+      mail(
+        :to => "#{user.name} <#{user.email}>",
+        :subject => "Está gostando do ContaCal?")
+      if (user.expire_at < Date.today-1)
+        user.update_attribute(:subscribed_daily, "f")
+      end
+    else  
+      mail(
+        :to => "#{user.name} <#{user.email}>",
+        :subject => "[ContaCal] Resumo de suas calorias em #{I18n.l(@date)}")
+      if (!user.nutri_email.blank?) 
+        mail(
+          :to => "#{user.nutri_name} <#{user.nutri_email}>",
+          :subject => "[ContaCal] Resumo das calorias de #{user.name} (#{user.email}) em #{I18n.l(@date)}")
+      end
+    end
+   end
   end
 
   def end_of_day(user)
--- lib/tasks/scheduler.rake
+++ lib/tasks/scheduler.rake
@@ -2,8 +2,8 @@ desc "This task is called by the Heroku scheduler add-on"
 
 task :beginning_of_day => :environment do
   User.send_beginning_of_day_notification!
-#  if Time.zone.now.wday == 1  
-#    User.send_weekly_notification!
-#  end
+  if Time.zone.now.wday == 1  
+    User.send_weekly_notification!
+  end
 end
 
