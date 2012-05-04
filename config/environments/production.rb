dcb9e76683256671bf8457a46f6de5bf6eb5570e
Rachad Honein <rachad.honein@locaweb.com.br>
2012-05-04 19:54:46 -0300

    config email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,12 +50,19 @@ Contacal::Application.configure do
   
   config.action_mailer.default_url_options = { :host => 'app.contacal.com.br' }
   
+# config.action_mailer.smtp_settings = {
+#   :address        => "smtp.sendgrid.net",
+#   :port           => "25",
+#   :authentication => :plain,
+#   :user_name      => ENV['SENDGRID_USERNAME'],
+#   :password       => ENV['SENDGRID_PASSWORD'],
+#   :domain         => ENV['SENDGRID_DOMAIN']
+# }
+
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
-    :port           => "25",
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => ENV['SENDGRID_DOMAIN']
+      :address              => "localhost",
+      :port                 => 25
   }
+
 end
