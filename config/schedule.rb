a70b841e95df5aebf562e263048c5a3651c1006c
Rachad Honein <rachad.honein@locaweb.com.br>
2012-05-04 17:02:55 -0300

    scheduler

---
+++ config/schedule.rb
@@ -0,0 +1,30 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+#
+
+every 1.day, :at => '6:00 am' do
+  runner "User.send_beginning_of_day_notification!"
+end
+
+every :monday, :at =>'6:00am' do
+  runner "User.send_weekly_notification!"
+end
+
