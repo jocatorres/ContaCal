8426764d1639459b633f2d5c7a570d77204dc34d
Joca Torres <jtorres@jig.com.br>
2013-02-13 23:46:28 -0200

    Mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,10 @@ class ApplicationController < ActionController::Base
       "devise"
     else
       "application"
-    end
+    end     
   end
+         
+  before_filter :prepare_for_mobile
 
   def mobile_device?
     if session[:mobile_param]
@@ -24,6 +26,10 @@ class ApplicationController < ActionController::Base
       request.user_agent =~ /Mobile|webOS/
     end
   end
-
   helper_method :mobile_device?
+  
+  def prepare_for_mobile
+    session[:mobile_param] = params[:mobile] if params[:mobile]
+    request.format = :mobile if mobile_device?
+  end
 end
