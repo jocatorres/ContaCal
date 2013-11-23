ab8cf5b8692da639778ded01566186c3cd6fd93a
Joca Torres <jtorres@jig.com.br>
2013-11-23 07:50:22 -0200

    upgrade ruby version

--- app/controllers/user_friends_controller.rb
+++ app/controllers/user_friends_controller.rb
@@ -4,21 +4,21 @@ class UserFriendsController < ApplicationController
   layout 'devise'
   
   def new
-    @user_friends = UserFriends.new
+    @user_friend = UserFriends.new
   end
   
   def create
-    @user_frineds = UserFriends.new(params[:food])
+    @user_frined = UserFriends.new(params[:user_friend])
     
-    if (!@user_friends.name.blank? && !@user_friends.email.blank?)
-      NotificationMailer.new_food(current_user, @user_friends).deliver  
-      @user_friends.name.gsub!('+', ' ')
-      @user_friends.update_attribute(:kind, "h")      
-      @user_friends.update_attribute(:suggester_id, current_user.id)
-      @user_friends.save
-      redirect_to new_user_friends_path, notice: '<center>Sugestão enviada com sucesso!</center>'
+    if (!@user_friend.name.blank? && !@user_friend.email.blank?)
+      NotificationMailer.new_user_friend(current_user, @user_friend).deliver  
+      @user_friend.name.gsub!('+', ' ')
+      @user_friend.update_attribute(:kind, "h")      
+      @user_friend.update_attribute(:suggester_id, current_user.id)
+      @user_friend.save
+      redirect_to new_user_friend_path, notice: '<center>Sugestão enviada com sucesso!</center>'
     else
-      @user_friends.errors.add(:name, :empty)
+      @user_friend.errors.add(:name, :empty)
       render action: "new"
     end    
   end
