26333702c90e5ae7c47b27f489d0756c8ef2a9ad
Cristina Conacel <cristina@cowboy.com>
2023-05-03 12:24:21 +0300

    Fixed: routing tests.

--- spec/routing/dashboard_routing_spec.rb
+++ spec/routing/dashboard_routing_spec.rb
@@ -1,40 +1,40 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
-describe DashboardController do
+describe DashboardController, type: :routing do
   describe "routing" do
     it "recognizes and generates index" do
-      get("/").should route_to("dashboard#index")
-      root_path.should == '/'
+      expect({ get: "/" }).to route_to("dashboard#index")
+      expect(root_path).to eq('/')
     end
 
     it "recognizes and generates authenticate" do
-      get("/authenticate").should route_to("dashboard#authenticate")
-      authenticate_path.should == '/authenticate'
+      expect({ get: "/authenticate" }).to route_to("dashboard#authenticate")
+      expect(authenticate_path).to eq('/authenticate')
     end
 
     it "recognizes and generates index with date" do
-      get("/2011/08/24").should route_to("dashboard#index", :year => '2011', :month => '08', :day => '24')
-      dashboard_path(:year => '2011', :month => '08', :day => '24').should == '/2011/08/24'
+      expect({ get: "/2011/08/24" }).to route_to("dashboard#index", :year => '2011', :month => '08', :day => '24')
+      expect(dashboard_path(:year => '2011', :month => '08', :day => '24')).to eq('/2011/08/24')
     end
 
     it "recognizes and generates kcal_limit" do
-      put("/kcal_limit").should route_to("dashboard#update_kcal_limit")
-      kcal_limit_path.should == '/kcal_limit'
+      expect({ put: "/kcal_limit" }).to route_to("dashboard#update_kcal_limit")
+      expect(kcal_limit_path).to eq('/kcal_limit')
     end
 
     it "recognizes and generates user_weight" do
-      put("/user_weight").should route_to("dashboard#update_user_weight")
-      weight_path.should == '/user_weight'
+      expect({ put: "/user_weight" }).to route_to("dashboard#update_user_weight")
+      expect(user_weight_path).to eq('/user_weight')
     end
 
     it "recognizes and generates confirm" do
-      get("/autocomplete_food_name").should route_to("dashboard#autocomplete_food_name")
-      autocomplete_food_name_path.should == "/autocomplete_food_name"
+      expect({ get: "/autocomplete_food_name" }).to route_to("dashboard#autocomplete_food_name")
+      expect(autocomplete_food_name_path).to eq("/autocomplete_food_name")
     end
 
     it "recognizes and generates report" do
-      get("/report").should route_to("dashboard#report")
-      report_path.should == "/report"
+      expect({ get: "/report" }).to route_to("dashboard#report")
+      expect(report_path).to eq("/report")
     end
   end
 end
--- spec/routing/user_foods_routing_spec.rb
+++ spec/routing/user_foods_routing_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
-describe UserFoodsController do
+describe UserFoodsController, type: :routing do
   describe "routing" do
     it "recognizes and generates update" do
-      put("/user_foods/1").should route_to("user_foods#update", :id => "1")
-      user_food_path(1).should == '/user_foods/1'
+      expect({ put: "/user_foods/1" }).to route_to("user_foods#update", :id => "1")
+      expect(user_food_path(1)).to eq('/user_foods/1')
     end
     it "recognizes and generates create" do
-      post("/user_foods").should route_to("user_foods#create")
-      user_foods_path.should == '/user_foods'
+      expect({ post: "/user_foods" }).to route_to("user_foods#create")
+      expect(user_foods_path).to eq('/user_foods')
     end
     it "recognizes and generates destroy" do
-      delete("/user_foods/1").should route_to("user_foods#destroy", :id => "1")
-      user_food_path(1).should == '/user_foods/1'
+      expect({ delete: "/user_foods/1" }).to route_to("user_foods#destroy", :id => "1")
+      expect(user_food_path(1)).to eq('/user_foods/1')
     end
   end
 end
