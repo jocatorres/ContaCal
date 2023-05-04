0796bf7deceadc68aa7aca494857c96e4c9dd37c
Cristina Conacel <elena.cristina.conacel@gmail.com>
2023-05-04 16:45:16 +0300

    Changed: external links using http instead of https. Changed: order of js assets so jquery is first.

--- app/assets/javascripts/all.js
+++ app/assets/javascripts/all.js
@@ -1,7 +1,7 @@
-//= require rails
-//= require autocomplete-rails
 //= require jquery
+//= require rails
 //= require jquery.fancybox-1.3.4.pack
 //= require jquery-ui-1.8.16.custom.min
+//= require autocomplete-rails
 //= require jquery.debug
 //= require application
