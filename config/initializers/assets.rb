c0a27a443895d3230695b1ce7b821e48c6e5822b
Cristina Conacel <cristina@cowboy.com>
2023-05-04 16:06:42 +0300

    Added: config to precompile existing js and css.

---
+++ config/initializers/assets.rb
@@ -0,0 +1,3 @@
+Contacal::Application.configure do
+	config.assets.precompile += ['all.js', 'contacal.css', 'contacal.mobile.css', 'flash.css', 'flash.mobile.css','iefix.css', 'iefix.mobile.css',  'jquery-ui.css', 'jquery.fancybox-1.3.4.css', 'reset.css']
+end
