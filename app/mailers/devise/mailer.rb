091a45568f9452a1ee272ce2e4f2598c932462e5
Cristina Conacel <elena.cristina.conacel@gmail.com>
2023-05-05 16:35:41 +0300

    Changed: saving the token in an instance variable.

--- app/mailers/devise/mailer.rb
+++ app/mailers/devise/mailer.rb
@@ -2,11 +2,12 @@ class Devise::Mailer < ::ActionMailer::Base
   include Devise::Mailers::Helpers
   layout 'mailer'
 
-  def reset_password_instructions(record, token, opts = {})
-    devise_mail(record, :reset_password_instructions)
+  def reset_password_instructions(record, token, opts={})
+    @token = token
+    devise_mail(record, :reset_password_instructions, opts)
   end
 
-  def unlock_instructions(record, token, opts = {})
+  def unlock_instructions(record)
     devise_mail(record, :unlock_instructions)
   end
 end
